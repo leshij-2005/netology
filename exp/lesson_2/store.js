434ca05cb85a8c9fdc40cb39763d96697ef8ccdc
Alexey Ershov <alershov3@gmail.com>
2017-03-05 20:31:44 +0500

    rename r -> rental

--- exp/lesson_2/store.js
+++ exp/lesson_2/store.js
@@ -5,25 +5,25 @@ function statement(customer, movies) {
 	let totalFrequentRenterPoints = 0;
 	let result = `Rental Record for ${customer.name}\n`;
 
-	for (let r of customer.rentals) {
-		let movie = movies[r.movieID];
+	for (let rental of customer.rentals) {
+		let movie = movies[rental.movieID];
 		let thisAmount = 0;
 
 		// determine amount for each movie
 		switch (movie.code) {
 			case "regular":
 				thisAmount = 2;
-				if (r.days > 2) {
-					thisAmount += (r.days - 2) * 1.5;
+				if (rental.days > 2) {
+					thisAmount += (rental.days - 2) * 1.5;
 				}
 				break;
 			case "new":
-				thisAmount = r.days * 3;
+				thisAmount = rental.days * 3;
 				break;
 			case "childrens":
 				thisAmount = 1.5;
-				if (r.days > 3) {
-					thisAmount += (r.days - 3) * 1.5;
+				if (rental.days > 3) {
+					thisAmount += (rental.days - 3) * 1.5;
 				}
 				break;
 		}
@@ -32,7 +32,7 @@ function statement(customer, movies) {
 		totalFrequentRenterPoints++;
 
 		// add bonus for a two day new release rental
-		if (movie.code === "new" && r.days > 2) {
+		if (movie.code === "new" && rentalr.days > 2) {
 			totalFrequentRenterPoints++;
 		}
 
