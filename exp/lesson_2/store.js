eabb8df99d0a80f245b40615b09906de0db00c09
Alexey Ershov <alershov3@gmail.com>
2017-03-05 21:10:05 +0500

    clear code

--- exp/lesson_2/store.js
+++ exp/lesson_2/store.js
@@ -1,18 +1,14 @@
-"use strict";
+'use strict';
 
 function statement(customer, movies) {
-	let result = `Rental Record for ${customer.name}\n`;
-
 	let totalFrequentRenterPoints = getTotalFrequentRenterPoints(customer);
-
 	let totalAmount = getTotalAmount(customer);
 
+	let result = `Rental Record for ${customer.name}\n`;
 	for (let rental of customer.rentals) {
 		let movie = movieFor(rental);
-		let thisAmount = getAmount(rental);
 
-		//print figures for this rental
-		result += `\t${movie.title}\t${thisAmount}\n`;
+		result += `\t${movie.title}\t${getAmount(rental)}\n`;
 	}
 
 	// add footer lines
@@ -31,16 +27,16 @@ function statement(customer, movies) {
 
 		// determine amount for each movie
 		switch (movie.code) {
-			case "regular":
+			case 'regular':
 				thisAmount = 2;
 				if (rental.days > 2) {
 					thisAmount += (rental.days - 2) * 1.5;
 				}
 				break;
-			case "new":
+			case 'new':
 				thisAmount = rental.days * 3;
 				break;
-			case "childrens":
+			case 'childrens':
 				thisAmount = 1.5;
 				if (rental.days > 3) {
 					thisAmount += (rental.days - 3) * 1.5;
@@ -54,7 +50,7 @@ function statement(customer, movies) {
 	function getTotalFrequentRenterPoints(customer) {
 		let total = 0;
 		for (let rental of customer.rentals) {
-			total += (movieFor(rental).code === "new" && rental.days > 2) ? 2 : 1;
+			total += (movieFor(rental).code === 'new' && rental.days > 2) ? 2 : 1;
 		}
 
 		return total;
@@ -72,26 +68,25 @@ function statement(customer, movies) {
 }
 
 let customer = {
-	name: "martin",
+	name: 'martin',
 	rentals: [{
-		"movieID": "F001",
-		"days": 3
+		movieID: 'F001',
+		days: 3
 	}, {
-		"movieID": "F002",
-		"days": 1
+		movieID: 'F002',
+		days: 1
 	}]
 };
 
 let movies = {
-	"F001": {
-		"title": "Ran",
-		"code": "regular"
+	F001: {
+		title: 'Ran',
+		code: 'regular'
 	},
-	"F002": {
-		"title": "Trois Couleurs: Bleu",
-		"code": "regular"
+	F002: {
+		title: 'Trois Couleurs: Bleu',
+		code: 'regular'
 	}
-	// etc
 };
 
 console.log(statement(customer, movies));
