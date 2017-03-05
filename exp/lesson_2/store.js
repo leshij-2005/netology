6c1760f72077c33d03576916cceb72c51a268042
Alexey Ershov <alershov3@gmail.com>
2017-03-05 20:50:20 +0500

    refactor calculateFrequentRenterPoints

--- exp/lesson_2/store.js
+++ exp/lesson_2/store.js
@@ -54,16 +54,7 @@ function statement(customer, movies) {
 	}
 
 	function calculateFrequentRenterPoints(rental) {
-		let movie = movieFor(rental);
-		let totalFrequentRenterPoints = 0;
-
-		//add frequent renter points
-		totalFrequentRenterPoints++;
-
-		// add bonus for a two day new release rental
-		if (movie.code === "new" && rental.days > 2) {
-			totalFrequentRenterPoints++;
-		}
+		return (movieFor(rental).code === "new" && rental.days > 2) ? 2 : 1;
 	}
 }
 
