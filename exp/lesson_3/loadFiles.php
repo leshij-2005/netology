2b9789bc37e26ede4cbbb8e9fe6dfee3ce502d3d
Alexey Ershov <alershov3@gmail.com>
2017-03-05 23:58:13 +0500

    clear code;

--- exp/lesson_3/loadFiles.php
+++ exp/lesson_3/loadFiles.php
@@ -18,35 +18,35 @@
   }
 
   function resizeImage($path, $width, $height) {
+    $ratio = $width / $height;
+
     list($width_orig, $height_orig) = getimagesize($path);
+    $ratio_orig = $width_orig / $height_orig;
 
-    $ratio_orig = $width_orig/$height_orig;
-
-    if ($width/$height > $ratio_orig) {
-      $width = $height*$ratio_orig;
-    } else {
-      $height = $width/$ratio_orig;
-    }
+    $width = $ratio > $ratio_orig ? $height * $ratio_orig : $width;    
+    $height = $ratio < $ratio_orig ? $width / $ratio_orig : $height;
 
     $image_p = imagecreatetruecolor($width, $height);
     $image = imagecreatefromjpeg($path);
-    imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
 
-    return imagejpeg($image_p, $path, 100);
+    if (imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig)) {
+      return imagejpeg($image_p, $path, 100);
+    }
+
+    return false;
   }
+
+  $params = array();
 
   if (count($_FILES) > 0)
   {
-    $width = 800;
-    $height = 600;
-
     foreach($_FILES as $key => $file)
     {
-      $fileName = makeFileName($file, $filePath);
+      $fileName = makeFileName($file);
       $filePath = $_SERVER['DOCUMENT_ROOT']."/assets/resources/$fileName";
 
-      if (loadFile($file)) {
-        resizeImage($filePath);
+      if (loadFile($file, $filePath)) {
+        resizeImage($filePath, 800, 600);
 
         $params[$key] = "/assets/resources/$fileName";
       }
