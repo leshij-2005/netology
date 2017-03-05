40c5fb80d47b67f97e8973b4922580387710d8e9
Alexey Ershov <alershov3@gmail.com>
2017-03-05 23:37:32 +0500

    add homework for lesson 3

---
+++ exp/lesson_3/loadFiles.php
@@ -0,0 +1,42 @@
+<?php
+  if (count($_FILES) > 0)
+  {
+    $width = 800;
+    $height = 600;
+
+    foreach($_FILES as $key => $file)
+    {
+      if ($file['name'])
+      {
+        $fn = explode(".", $file['name']);
+        $f = $fn[0].time().".".$fn[1];
+
+        if(is_uploaded_file($file['tmp_name']))
+        {
+          $filename = $_SERVER['DOCUMENT_ROOT']."/assets/resources/$f";
+
+          if (move_uploaded_file($file['tmp_name'], $filename)) {
+            $resource = "/assets/resources/$f";
+
+            list($width_orig, $height_orig) = getimagesize($filename);
+
+          $ratio_orig = $width_orig/$height_orig;
+
+          if ($width/$height > $ratio_orig) {
+             $width = $height*$ratio_orig;
+          } else {
+             $height = $width/$ratio_orig;
+          }
+
+          $image_p = imagecreatetruecolor($width, $height);
+          $image = imagecreatefromjpeg($filename);
+          imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
+
+          imagejpeg($image_p, $filename, 100);
+
+          $params['logo'] = $resource;
+          }
+        }
+      }
+    }
+  }
