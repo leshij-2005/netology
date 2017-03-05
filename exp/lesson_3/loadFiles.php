40988ad12e77eaa76cfd9f2e0ee756650747464c
Alexey Ershov <alershov3@gmail.com>
2017-03-05 23:51:40 +0500

    extract method resizeImage;

--- exp/lesson_3/loadFiles.php
+++ exp/lesson_3/loadFiles.php
@@ -10,30 +10,29 @@
     {
       if (is_uploaded_file($file['tmp_name']))
       {
-        if (move_uploaded_file($file['tmp_name'], $path)) {
-
-          list($width_orig, $height_orig) = getimagesize($path);
-
-          $ratio_orig = $width_orig/$height_orig;
-
-          if ($width/$height > $ratio_orig) {
-            $width = $height*$ratio_orig;
-          } else {
-            $height = $width/$ratio_orig;
-          }
-
-          $image_p = imagecreatetruecolor($width, $height);
-          $image = imagecreatefromjpeg($path);
-          imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
-
-          imagejpeg($image_p, $path, 100);
-
-          return true;
-        }
+        return move_uploaded_file($file['tmp_name'], $path);
       }
     }
 
     return false;
+  }
+
+  function resizeImage($path, $width, $height) {
+    list($width_orig, $height_orig) = getimagesize($path);
+
+    $ratio_orig = $width_orig/$height_orig;
+
+    if ($width/$height > $ratio_orig) {
+      $width = $height*$ratio_orig;
+    } else {
+      $height = $width/$ratio_orig;
+    }
+
+    $image_p = imagecreatetruecolor($width, $height);
+    $image = imagecreatefromjpeg($path);
+    imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
+
+    return imagejpeg($image_p, $path, 100);
   }
 
   if (count($_FILES) > 0)
@@ -43,10 +42,12 @@
 
     foreach($_FILES as $key => $file)
     {
-      $fileName = makeFileName($file);
+      $fileName = makeFileName($file, $filePath);
       $filePath = $_SERVER['DOCUMENT_ROOT']."/assets/resources/$fileName";
 
       if (loadFile($file)) {
+        resizeImage($filePath);
+
         $params[$key] = "/assets/resources/$fileName";
       }
     }
