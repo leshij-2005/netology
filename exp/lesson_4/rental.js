fe036df35d2f3f279a6abd7d8052f2edb75dd133
Alexey Ershov <alershov3@gmail.com>
2017-03-06 10:51:52 +0500

    add method amount in rental; inline getAmount

--- exp/lesson_4/rental.js
+++ exp/lesson_4/rental.js
@@ -19,6 +19,31 @@ class Rental {
   get frequentRentalPoints() {
     return (this.movie.code === "new" && this.days > 2) ? 2 : 1;
   }
+  
+  get amount() {
+    let movie = this.movie;
+    let amount = 0;
+    // determine amount for each movie
+    switch (movie.code) {
+      case "regular":
+        amount = 2;
+        if (this.days > 2) {
+          amount += (this.days - 2) * 1.5;
+        }
+        break;
+      case "new":
+        amount = this.days * 3;
+        break;
+      case "childrens":
+        amount = 1.5;
+        if (this.days > 3) {
+          amount += (this.days - 3) * 1.5;
+        }
+        break;
+    }
+  
+    return amount;
+  }
 }
 
 module.exports = Rental;
--- exp/lesson_4/store.js
+++ exp/lesson_4/store.js
@@ -2,31 +2,6 @@
 
 const Customer = require('./customer.js');
 
-function getAmount(rental) {
-  let movie = rental.movie;
-  let amount = 0;
-  // determine amount for each movie
-  switch (movie.code) {
-    case "regular":
-      amount = 2;
-      if (rental.days > 2) {
-        amount += (rental.days - 2) * 1.5;
-      }
-      break;
-    case "new":
-      amount = rental.days * 3;
-      break;
-    case "childrens":
-      amount = 1.5;
-      if (rental.days > 3) {
-        amount += (rental.days - 3) * 1.5;
-      }
-      break;
-  }
-  
-  return amount;
-}
-
 function getTotalFrequentRenterPoints(customer) {
   let totalFrequentRenterPoints = 0;
   for (let rental of customer.rentals) {
@@ -39,7 +14,7 @@ function getTotalFrequentRenterPoints(customer) {
 function getTotalAmount(customer) {
   let totalAmount = 0;
   for (let rental of customer.rentals) {
-    totalAmount += getAmount(rental);
+    totalAmount += rental.amount;
   }
   
   return totalAmount;
@@ -55,7 +30,7 @@ function txtStatement(customerArg, movies) {
   function buildBody() {
     let statement = '';
     for (let rental of customer.rentals) {
-      statement += `\t${rental.movie.title}\t${getAmount(rental)}\n`;
+      statement += `\t${rental.movie.title}\t${rental.amount}\n`;
     }
     
     return statement;
@@ -80,8 +55,7 @@ function htmlStatement(customerArg, movies) {
   const amount = () => getTotalAmount(customer);
   const frequentRenterPoints = () => getTotalFrequentRenterPoints(customer);
   const movie = (aRental) => aRental.movie;
-  const rentalAmount = (aRental) => getAmount(aRental);
-  
+  const rentalAmount = (aRental) => aRental.amount;
   let result = `<h1>Rental Record for <em>${customer.name}</em></h1>\n`;
   result += "<table>\n";
   for (let rental of customer.rentals) {
