8f8e74af84791b7433cb79d965d369a48dcc61ed
Alexey Ershov <alershov3@gmail.com>
2017-03-14 16:43:40 +0500

    reformat code;

--- exp/lesson_9/tests/when-barmen-pourswhisky-tests.js
+++ exp/lesson_9/tests/when-barmen-pourswhisky-tests.js
@@ -4,24 +4,24 @@ var Barmen = require('../src/barmen');
 var Visitor = require('../src/visitor');
 
 suite('when barmen pours whisky', function () {
-	let barmen = new Barmen();
-	let me = new Visitor();
-
-	setup(function () {
-		me.sober();
+  let barmen = new Barmen();
+  let me = new Visitor();
+  
+  setup(function () {
+    me.sober();
     barmen.free();
-	});
-
-	suite('i ask 50 grams', function () {
-		test('I get 50 grams', function () {
-			var iAskVolume = 50;
+  });
+  
+  suite('i ask 50 grams', function () {
+    test('I get 50 grams', function () {
+      var iAskVolume = 50;
       var whisky = 1;
-
-			var volumeInGlass = barmen.pour(whisky, iAskVolume);
-			
-			assert.equal(iAskVolume, volumeInGlass);
-		});
-	});
+      
+      var volumeInGlass = barmen.pour(whisky, iAskVolume);
+      
+      assert.equal(iAskVolume, volumeInGlass);
+    });
+  });
   
   suite('I get 50 grams whisky', function () {
     test('I drink whisky', function () {
@@ -42,26 +42,26 @@ suite('when barmen pours whisky', function () {
       assert.equal(false, me.isDrunk());
     });
   });
-
-	suite('i ask -10 grams', function () {
-		test('I get an error', function () {
-			var iAskVolume = -10;
+  
+  suite('i ask -10 grams', function () {
+    test('I get an error', function () {
+      var iAskVolume = -10;
       var whisky = 1;
-
-			expect(() => barmen.pour(whisky, iAskVolume)).to.throw(/Invalid volume of whisky/);
-		});
-	});
-
-	suite('i ask 500 grams', function () {
-		test('Barmen said there is no such glass', function () {
-			var iAskVolume = 500;
-			var whisky = 1;
-
-			expect(() => barmen.pour(whisky, iAskVolume)).to.throw(/There is no such glass/);
-		});
-	});
-
-	teardown(function () {
-
-	})
+      
+      expect(() => barmen.pour(whisky, iAskVolume)).to.throw(/Invalid volume of whisky/);
+    });
+  });
+  
+  suite('i ask 500 grams', function () {
+    test('Barmen said there is no such glass', function () {
+      var iAskVolume = 500;
+      var whisky = 1;
+      
+      expect(() => barmen.pour(whisky, iAskVolume)).to.throw(/There is no such glass/);
+    });
+  });
+  
+  teardown(function () {
+    
+  })
 });
