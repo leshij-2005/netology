e92931fe98f6886a53bc56f44e6037481220b9d4
Alexey Ershov <alershov3@gmail.com>
2017-03-25 20:01:08 +0500

    add test when barmen pours x3 volume on a birthday

--- exp/lesson_10/tests/when-barmen-pours-drinks.js
+++ exp/lesson_10/tests/when-barmen-pours-drinks.js
@@ -8,15 +8,23 @@ let Cupboard = require('../src/cupboard');
 suite('When barmen pours drinks', function () {
     let visitor = {};
     let barmen = {};
+    let calendar = {};
 
     setup(function () {
+        calendar = new CalendarStub();
         visitor = new Visitor();
         visitor.sober();
     });
 
     suite('cupboard is full', function () {
         test('barmen pours x3 volume on a birthday', function () {
+            barmen = new Barmen(new CupboardStub());
+            visitor.bithdate = '01.01.2017';
+            calendar.today = '01.01.2017';
 
+            const volumeInGlass = barmen.pour('beer', 100, visitor, calendar);
+
+            assert(300, volumeInGlass);
         });
     });
 });
