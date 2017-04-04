ec9a4581a4a340538724528b7ea464d856dc567c
Alexey Ershov <alershov3@gmail.com>
2017-04-04 16:20:55 +0500

    add test for case when bus is ready;

--- exp/lesson_13/tests/when-bus-left-on-route.js
+++ exp/lesson_13/tests/when-bus-left-on-route.js
@@ -1,5 +1,26 @@
 const assert = require('chai').assert;
 
 suite('when bus left on the route', function(){
+  test('bus is ready to receive passengers', function(){
+    const bus = new Bus();
+    
+    bus.setState('ready');
+    
+    assert.equal(bus.state, 'ready');
+  });
   
+  class Bus {
+    constructor() {
+      this._state = 'undefined';
+    }
+    
+    get state() {
+      return this._state;
+    }
+    
+    setState(state) {
+      this._state = state;
+    }
+  }
 });
+
