5c1963338c8e9875d9045c0eca07051d87aae403
Alexey Ershov <alershov3@gmail.com>
2017-04-04 16:31:31 +0500

    add test for case when bus receive passengers;

--- exp/lesson_13/src/bus.js
+++ exp/lesson_13/src/bus.js
@@ -1,14 +1,23 @@
 class Bus {
   constructor() {
     this._state = 'undefined';
+    this._passengers = [];
   }
   
   get state() {
     return this._state;
   }
   
+  get passengers() {
+    return this._passengers;
+  }
+  
   setState(state) {
     this._state = state;
+  }
+  
+  receive(passengers) {
+    this._passengers.push(...passengers);
   }
 }
 
--- exp/lesson_13/tests/when-bus-left-on-route.js
+++ exp/lesson_13/tests/when-bus-left-on-route.js
@@ -2,12 +2,26 @@ const assert = require('chai').assert;
 const Bus = require('../src/bus');
 
 suite('when bus left on the route', function(){
-  test('bus is ready to receive passengers', function(){
-    const bus = new Bus();
-    
+  let bus = {};
+  
+  setup(function() {
+    bus = new Bus();
+  
     bus.setState('ready');
-    
+  });
+  
+  test('bus is ready to receive passengers', function(){
     assert.equal(bus.state, 'ready');
   });
+  
+  test('bus receive one passenger', function() {
+    bus.receive([new Passenger()]);
+    
+    assert.equal(bus.passengers.length, 1);
+  });
+  
+  class Passenger {
+    
+  }
 });
 
