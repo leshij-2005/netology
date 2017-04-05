1cf6d8821caee71ffd2b7b3fb1738c5913bee043
Alexey Ershov <alershov3@gmail.com>
2017-04-05 15:15:28 +0500

    add busBuilder

--- exp/lesson_14/tests/when-bus-left-on-route.js
+++ exp/lesson_14/tests/when-bus-left-on-route.js
@@ -5,16 +5,35 @@ const Passenger = require('../src/passenger');
 suite('when bus left on the route', function(){
   let bus = {};
   
-  setup(function() {
-    bus = new Bus();
-  
-    bus.left();
-  });
-  
   test('when bus receive two passenger then bus is full', function() {
-    bus.receive([new Passenger(20), new Passenger(20)]);
+    const bus = new BusBuilder()
+      .receive(new Passenger(20))
+      .receive(new Passenger(20))
+      .go();
     
     assert.equal(bus.state, 'full');
   });
+  
+  class BusBuilder {
+    constructor() {
+      this._passengers = [];
+    }
+    
+    receive(passenger) {
+      this._passengers.push(passenger);
+      
+      return this;
+    }
+    
+    go() {
+      const bus = new Bus();
+      
+      bus.left();
+      
+      bus.receive(this._passengers);
+      
+      return bus;
+    }
+  }
 });
 
