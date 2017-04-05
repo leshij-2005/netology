fa262c8f5dde0afd60641e97c41a3bbb05a4090e
Alexey Ershov <alershov3@gmail.com>
2017-04-05 15:27:16 +0500

    add passengerBuilder; add test for case when passenger have`t money

--- exp/lesson_14/tests/when-bus-left-on-route.js
+++ exp/lesson_14/tests/when-bus-left-on-route.js
@@ -7,11 +7,48 @@ suite('when bus left on the route', function(){
   
   test('when bus receive two passenger then bus is full', function() {
     const bus = new BusBuilder()
-      .receive(new Passenger(20))
-      .receive(new Passenger(20))
+      .receive(new PassengerBuilder().withMoney(20).create())
+      .receive(new PassengerBuilder().withMoney(20).create())
       .go();
     
     assert.equal(bus.state, 'full');
   });
+  
+  test('when bus receive passenger and passenger have`t money then bus not receive passenger', function() {
+    const bus = new BusBuilder()
+      .receive(new PassengerBuilder().withNoMoney().create())
+      .go();
+    
+    assert.equal(bus.passengers.length, 0);
+  });
+  
+  class PassengerBuilder {
+    constructor() {
+      this._money = 0;
+      this._type = 'default';
+    }
+    
+    withMoney(money) {
+      this._money = money;
+      
+      return this;
+    }
+    
+    withNoMoney() {
+      this._money = 0;
+      
+      return this;
+    }
+    
+    withTypePensioner() {
+      this._type = 'pensioner';
+      
+      return this;
+    }
+    
+    create() {
+      return new Passenger(this._money, this._type);
+    }
+  }
 });
 
